235e2729cb9e79dd74c4e1784c7db72b09541610
oscar199996 <oscarcarcamo555@gmail.com>
2025-02-20 19:45:47 -0600

    Se agrego la carpeta Models

--- Data/RestauranteDbContext.cs
+++ Data/RestauranteDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace L01_2022CI650_2022VS601.Data
+﻿using Microsoft.EntityFrameworkCore;
+namespace L01_2022CI650_2022VS601.Data
 {
-    public class RestauranteDbContext
+    public class RestauranteDbContext : DbContext
     {
+        public RestauranteDbContext(DbContextOptions<RestauranteDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Plato> Platos { get; set; }
+        public DbSet<Motorista> Motoristas { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
+using L01_2022CI650_2022VS601.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<RestauranteDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +19,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
