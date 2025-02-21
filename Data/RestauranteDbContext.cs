dd0ac03140f1a3f484e98007ae1a7006ddf20d7b
oscar199996 <oscarcarcamo555@gmail.com>
2025-02-20 20:48:32 -0600

    arreglado el dbcontext

--- Data/RestauranteDbContext.cs
+++ Data/RestauranteDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using L01_2022CI650_2022VS601.Models
+
 namespace L01_2022CI650_2022VS601.Data
 {
     public class RestauranteDbContext : DbContext
     {
         public RestauranteDbContext(DbContextOptions<RestauranteDbContext> options) : base(options) { }
 
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Pedido> Pedidos { get; set; }
-        public DbSet<Plato> Platos { get; set; }
-        public DbSet<Motorista> Motoristas { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Pedidos> Pedidos { get; set; }
+        public DbSet<Platos> Platos { get; set; }
+        public DbSet<Motoristas> Motoristas { get; set; }
     }
 }
